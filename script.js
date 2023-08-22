10da98ec2fa31d78273244cbcb0f2510d1aedeaf
Torje Amundsen <torjefrafen@hotmail.no>
2023-08-22 15:02:35 +0200

    auto lights update

--- script.js
+++ script.js
@@ -5,33 +5,52 @@ const LIGHTS = [
 ];
 
 let currentLight = 0;
+let velocity = 1;
+let interval;
+
+const delay = (delayInms) => {
+    return new Promise(resolve => setTimeout(resolve, delayInms));
+}
 
 const allOff = () => {
+    clearInterval(interval);
     LIGHTS.forEach((e) => {
         e.classList.remove("on");
     });
-};
+}
 
 const allOn = () => {
+    clearInterval(interval);
     LIGHTS.forEach((e) => {
         e.classList.add("on");
     });
-};
+}
 
 const randomNum = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-// Positive direction = Red -> Green
-const setLight = (direction) => {
-    for (let i = 0; i < 2; i++) {
-        LIGHTS[currentLight].classList.toggle("on");
-        currentLight += direction;
-        LIGHTS[currentLight].classList.toggle("on");
-    };
-};
+}
 
 const specificLight = (index) => {
-    allOff();
+    clearInterval(interval);
     LIGHTS[index].classList.add("on");
-};
+    currentLight = index;
+}
+
+
+function toggleAutoLights() {
+    if (!running) {
+        specificLight(0);
+        velocity = 1;
+        interval = setInterval(autoLights, 1500);
+    } else {
+        clearInterval(interval);
+    }
+}
+
+function autoLights() {
+    if (!(currentLight === 0 && velocity === 1)) LIGHTS[currentLight].classList.toggle("on");
+    if (currentLight + velocity === 2) LIGHTS[0].classList.toggle("on");
+    currentLight += velocity;
+    LIGHTS[currentLight].classList.toggle("on");
+    velocity = currentLight == 2 || currentLight == 0 ? velocity * -1 : velocity;
+}
